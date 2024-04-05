93593dbdbe96614b8d291b8d648874cb6fcb2cb3
Ajay K <157508916+AjayK10422@users.noreply.github.com>
2024-04-04 21:39:17 -0400

    Delete Capstone directory

--- Capstone/NextwordPredictor/myapp/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class InputText(models.Model):
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
--- Capstone/NextwordPredictor/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.input_text, name='input_text'),
-    
-]
